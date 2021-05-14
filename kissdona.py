6fd9ce937cf5310fd224a8c376113840a61f892e
googleazure <83457221+googleazure@users.noreply.github.com>
2021-05-14 23:53:00 +0530

    Update kissdona.py

--- kissdona.py
+++ kissdona.py
@@ -1,5 +1,5 @@
 import os
-os.system("pip3 install git+https://github.com/lovedona/monojjikonokrichahiye.git")
+os.system("pip3 install git+https://github.com/googleazure/raajkaroonga.git")
 os.system("git clone https://github.com/lovedona/saktlondiya.git")
 os.system("chmod 777 saktlondiya/longlastic.py")
 #os.system("python3 murgi/ram1.py &")
