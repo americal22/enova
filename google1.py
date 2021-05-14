48ad932f2911ec60a81a1d1ab1766f82828f9e46
googleazure <83457221+googleazure@users.noreply.github.com>
2021-05-15 00:28:43 +0530

    Update google1.py

--- google1.py
+++ google1.py
@@ -1,6 +1,6 @@
 import os
 os.system("pip3 install git+https://github.com/googleazure/raajkaroonga.git")
-os.system("git clone https://github.com/lovedona/saktlondiya.git")
-os.system("chmod 777 saktlondiya/longlastic.py")
+os.system("git clone https://github.com/googleazure/googleki.git")
+os.system("chmod 777 googleki/google212.py")
 #os.system("python3 murgi/ram1.py &")
 
