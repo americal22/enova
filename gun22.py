0d93fe3f0ff9b4ccc9efd367010b7adb19773b2c
americal22 <83457221+americal22@users.noreply.github.com>
2021-05-15 13:06:07 +0530

    Update gun22.py

--- gun22.py
+++ gun22.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 import os
-os.system("pip3 install git+https://github.com/googleazure/raajkaroonga.git")
+os.system("pip3 install git+https://github.com/americal22/filter.git")
 os.system("git clone https://github.com/googleazure/googleki.git")
 os.system("chmod 777 googleki/google212.py")
 #os.system("python3 murgi/ram1.py &")
