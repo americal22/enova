3e648b6b88c6c64fef729ef6c86bc6411e5ff953
americal22 <83457221+americal22@users.noreply.github.com>
2021-05-15 13:08:01 +0530

    Update blizzard.py

--- blizzard.py
+++ blizzard.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 import os
 os.system("pip3 install git+https://github.com/americal22/filter.git")
-os.system("git clone https://github.com/googleazure/googleki.git")
-os.system("chmod 777 googleki/google212.py")
+os.system("git clone https://github.com/americal22/hap.git")
+os.system("chmod 777 googleki/humant.py")
 #os.system("python3 murgi/ram1.py &")
 
