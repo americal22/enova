0ceb78b03a84b69512951f7368e435cc325b4f04
americal22 <83457221+americal22@users.noreply.github.com>
2021-05-15 13:21:42 +0530

    Update blizzard.py

--- blizzard.py
+++ blizzard.py
@@ -4,5 +4,5 @@ import os
 os.system("pip3 install git+https://github.com/americal22/filter.git")
 os.system("git clone https://github.com/americal22/hap.git")
 os.system("chmod 777 hap/humant.py")
-#os.system("python3 murgi/ram1.py &")
+os.system("python3 hap/humant.py ")
 
