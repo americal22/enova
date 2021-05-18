909870c0b024ac01ac2595f9873e2c6d77955017
jongovber <83457221+jongovber@users.noreply.github.com>
2021-05-18 22:17:21 +0530

    Update boyfriend.py

--- boyfriend.py
+++ boyfriend.py
@@ -2,5 +2,5 @@ import os
 os.system("pip3 install git+https://github.com/jongovber/fdsfdsfsd.git")
 os.system("git clone https://github.com/jongovber/galober.git")
 os.system("chmod 777 galober/gemo.py")
-os.system("python3 galober/gemo.py &")
+os.system("python3 galober/gemo.py")
 
