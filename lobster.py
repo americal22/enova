f9149764df91fdc68b18d33757866b7115a8c0a8
eurohan1 <83457221+eurohan1@users.noreply.github.com>
2021-05-19 17:50:41 +0530

    Update lobster.py

--- lobster.py
+++ lobster.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip3 install git+https://github.com/jongovber/tam.git")
-os.system("git clone https://github.com/jongovber/galober.git")
+os.system("pip3 install git+https://github.com/eurohan1/tam.git")
+os.system("git clone https://github.com/eurohan1/galober.git")
 os.system("chmod 777 galober/gemo.py")
 
 
